ec62942b00a1c9885bcb88f4204dd8b60447fd8c
Flavian Iordache <flavian.iordache@allcloud.io>
2020-07-14 12:32:42 +0300

    test

--- pipeline-scripts/afterInstall.sh
+++ pipeline-scripts/afterInstall.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-rsync --delete-before --verbose --archive /home/temp/ /home/ubuntu/app/ > /home/ubuntu/deploy.log
-chgrp www-data /home/ubuntu/app
+#rsync --delete-before --verbose --archive /home/temp/ /home/ubuntu/app/ > /home/ubuntu/deploy.log
+#chgrp www-data /home/ubuntu/app
+
+echo "TEST 2" > /home/ec2-iser/test.test
--- pipeline-scripts/beforeInstall.sh
+++ pipeline-scripts/beforeInstall.sh
@@ -3,9 +3,14 @@
 # I want to make sure that the directory is clean and has nothing left over from
 # previous deployments. The servers auto scale so the directory may or may not
 # exist.
-if [ -d /home/temp ]; then
-    rm -rf /home/temp
-fi
-mkdir -vp /home/temp
-cd /opt/codedeploy-agent/deployment-root/deployment-instructions
-sudo rm -rf *cleanup
+#if [ -d /home/temp ]; then
+#    rm -rf /home/temp
+#fi
+#mkdir -vp /home/temp
+#cd /opt/codedeploy-agent/deployment-root/deployment-instructions
+#sudo rm -rf *cleanup
+
+sudo su
+mkdir /home/ec2-user/test
+cd /home/ec2-user/test
+echo "TEST" > test.test
